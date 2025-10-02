7078a2d38a95ea8614c34ddc1402505a9e4aca81
jmabagnotan <juliemaebagnotan@gmail.com>
2025-10-02 14:00:27 +0800

    Fixed the sign in authentication

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,15 @@
-
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY || "AIzaSyDp8Tds3R9QnezVGqNNbJWIjSxshSFGgsM",
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN || "recipedia-a37b0.firebaseapp.com",
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID || "recipedia-a37b0",
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET || "recipedia-a37b0.appspot.com",
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID || "459895287309",
-  appId: process.env.REACT_APP_FIREBASE_APP_ID || "1:459895287309:web:2b6ddca1901172bd05af44",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY || "AIzaSyBKDc1GQtWAdnxRv5VHcHHJwJuPS8LZO2A",
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN || "recipe-palette-v1.firebaseapp.com",
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID || "recipe-palette-v1",
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET || "recipe-palette-v1.appspot.com", 
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID || "662894538527",
+  appId: process.env.REACT_APP_FIREBASE_APP_ID || "1:662894538527:web:9917d42102ae1d684aca2a",
 };
+
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export default app;
